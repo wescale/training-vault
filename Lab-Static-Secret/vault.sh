34ae45cdcb671831612bb50f68587e0e89d507f0
Jérôme Masson - Sphinxgaia <sphinxgaia@jeromemasson.fr>
2024-05-27 14:43:16 +0200

    fix: Lab-static-secrets: fixes vault init

--- Lab-Static-Secret/vault.sh
+++ Lab-Static-Secret/vault.sh
@@ -12,15 +12,16 @@ docker network connect pg server01
 docker network connect pg postgres
 
 sleep 5
-vault operator init -key-shares=1 -key-threshold=1 > key.txt
+vault operator init -key-shares=1 -key-threshold=1 > ../vault-key.txt
 
 sleep 2
 
-vault operator unseal $(grep 'Key 1:' key.txt | awk '{print $NF}')
+vault operator unseal $(grep 'Key 1:' ../vault-key.txt | awk '{print $NF}')
+cp ../vault-key.txt ./vault-init.txt
 
 sleep 2
 
-vault login $(grep 'Initial Root Token:' key.txt | awk '{print $NF}')
+vault login $(grep 'Initial Root Token:' ../vault-key.txt | awk '{print $NF}')
 
 sudo mkdir -p vault01/file/audit
 sudo chown -R 100:1000 vault01/file/audit
