75c51b7b5a22478d06a6aba3d4b3e06464d013ca
Jérôme Masson - Sphinxgaia <sphinxgaia@jeromemasson.fr>
2024-06-14 09:39:53 +0200

    feat: global install management

---
+++ cleanup-install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export ROOT_DIR_PATH=$(dirname $(realpath "$0"))
+
+sudo rm -rf $ROOT_DIR_PATH/vault01/file/audit || true
+
+sudo docker container rm -f $(docker container ls -aq) || true
+
+sudo rm $ROOT_DIR_PATH/vault-key.txt || true
+sudo rm $ROOT_DIR_PATH/vault-init.txt || true
+
+unset VAULT_TOKEN
